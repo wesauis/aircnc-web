bdbaf98e58ae9aeab765d57459deb08904c78f1d
wesauis <wesleya.isotton@gmail.com>
2019-10-06 11:56:08 -0300

    start of email form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 const App: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <img src={logo} alt="" />
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre{' '}
+          <strong>talentos</strong> para sua empresa
+        </p>
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+        </form>
+        <input type="email" id="email" placeholder="Seu melhor e-mail" />
+      </div>
+    </div>
+  );
 };
 
 export default App;
