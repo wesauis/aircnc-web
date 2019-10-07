583da62ff02693b48ab62fb3da9c078a1630f7cf
W2GOfficial <wesleya.isotton@gmail.com>
2019-10-06 23:23:28 -0300

    user login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from './assets/logo.svg';
 import './App.css';
 
-import logo from './assets/logo.svg';
+import api from './services/api';
 
 const App: React.FC = () => {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const res = await api.post('/sessions', { email });
+    const { id } = res.data;
+
+    localStorage.setItem('aircnc.user_id', id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="" />
       <div className="content">
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre{' '}
-          <strong>talentos</strong> para sua empresa
+          <strong>talentos</strong> para sua empresa'
         </p>
-        <form>
+
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <button className="btn" type="submit">
+            Entrar
+          </button>
         </form>
-        <input type="email" id="email" placeholder="Seu melhor e-mail" />
       </div>
     </div>
   );
