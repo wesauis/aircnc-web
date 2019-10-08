950a3b9ccf6bd2a78f0159df50d9b9fad3cc21e1
W2GOfficial <wesleya.isotton@universo.univates.br>
2019-10-07 22:50:53 -0300

    approving and rejecting solicitations

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import './styles.css';
 
 import api from '../../services/api';
+import { request } from 'https';
 
 interface ISpot {
   _id: string;
@@ -13,6 +15,22 @@ interface ISpot {
 
 const Dashboard: React.FC = () => {
   const [spots, setSpots] = useState<ISpot[]>([]);
+  const [requests, setRequests] = useState<any[]>([]);
+
+  const user_id = localStorage.getItem('aircnc.user_id');
+  const socket = useMemo(
+    () =>
+      socketio(String(process.env.REACT_APP_API_BASEURL), {
+        query: { user_id },
+      }),
+    [user_id],
+  );
+
+  useEffect(() => {
+    socket.on('booking_request', (data: any) => {
+      setRequests([...requests, data]);
+    });
+  }, [requests, socket]);
 
   useEffect(() => {
     (async () => {
@@ -24,8 +42,38 @@ const Dashboard: React.FC = () => {
     })();
   }, []);
 
+  async function handleSolicitation(id: number, approved: boolean) {
+    if (approved) await api.post(`/bookings/${id}/approvals`);
+    else await api.post(`/bookings/${id}/rejections`);
+    setRequests(requests.filter(req => req._id !== id));
+  }
+
   return (
     <>
+      <ul className="notifications">
+        {requests.map((req: any) => (
+          <li key={req._id}>
+            <p>
+              <strong>{req.user.email}</strong> está solicitando uma reserva em{' '}
+              <strong>{req.spot.company}</strong> para a data{' '}
+              <strong>{req.date}</strong>
+            </p>
+            <button
+              className="accept"
+              onClick={() => handleSolicitation(req._id, true)}
+            >
+              ACEITAR
+            </button>
+            <button
+              className="reject"
+              onClick={() => handleSolicitation(req._id, false)}
+            >
+              REGEITAR
+            </button>
+          </li>
+        ))}
+      </ul>
+
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
